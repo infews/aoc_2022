9d3c640ad93de75a6d08307a25c32f96585c9a03
Davis W. Frank <dwfrank@gmail.com>
2022-12-02 13:37:44 -0800

    Refactor to 2 game classes, 1 RPS class

--- lib/aoc_2022/day02.rb
+++ lib/aoc_2022/day02.rb
@@ -1,46 +1,38 @@
 module Aoc2022
   module Day02
-    class RockPaperScissorsPart1
+    class RockPaperScissors
       def initialize(input)
-        @games = input.split("\n")
-          .collect { |line| StraightGame.new(line) }
+        @lines = input.split("\n")
       end
 
-      def total_score
+      def straight_score
+        @games = @lines.collect { |line| StraightGame.new(line) }
+
+        @games.sum { |g| g.score }
+      end
+
+      def strat_score
+        @games = @lines.collect { |line| StratGame.new(line) }
         @games.sum { |g| g.score }
       end
     end
 
     class StraightGame
-      RESULT = {
-        "A X" => :tie, "A Y" => :win, "A Z" => :loss,
-        "B X" => :loss, "B Y" => :tie, "B Z" => :win,
-        "C X" => :win, "C Y" => :loss, "C Z" => :tie
+      STRAIGHT_RESULT = {
+        "A X" => 4, "A Y" => 8, "A Z" => 3,
+        "B X" => 1, "B Y" => 5, "B Z" => 9,
+        "C X" => 7, "C Y" => 2, "C Z" => 6
       }
-      PLAY = {"X" => 1, "Y" => 2, "Z" => 3}
-      SCORING = {win: 6, loss: 0, tie: 3}
 
       attr_reader :score
 
-      def initialize(plays)
-        result = RESULT[plays]
-        @score = PLAY[plays.split(" ").last] + SCORING[result]
+      def initialize(line)
+        @score = STRAIGHT_RESULT[line]
       end
     end
 
-    class RockPaperScissorsPart2
-      def initialize(input)
-        @games = input.split("\n")
-          .collect { |line| Game.new(line) }
-      end
-
-      def total_score
-        @games.sum { |g| g.score }
-      end
-    end
-
-    class Game
-      RESULT = {
+    class StratGame
+      STRAT_RESULT = {
         "A X" => 3, "A Y" => 4, "A Z" => 8,
         "B X" => 1, "B Y" => 5, "B Z" => 9,
         "C X" => 2, "C Y" => 6, "C Z" => 7
@@ -49,7 +41,7 @@ module Aoc2022
       attr_reader :score
 
       def initialize(line)
-        @score = RESULT[line]
+        @score = STRAT_RESULT[line]
       end
     end
   end
--- spec/aoc_2022/day02_spec.rb
+++ spec/aoc_2022/day02_spec.rb
@@ -1,54 +1,55 @@
-RSpec.describe Aoc2022::Day02::RockPaperScissorsPart1 do
-  let(:subject) { Aoc2022::Day02::RockPaperScissorsPart1.new(input) }
+RSpec.describe Aoc2022::Day02::RockPaperScissors do
+  let(:subject) { Aoc2022::Day02::RockPaperScissors.new(input) }
 
-  context "solve for test data" do
-    let(:input) {
-      %(A Y
+  let(:test_data) {
+    %(A Y
 B X
 C Z
 )
-    }
-    describe "#total_score" do
-      it "solves part I" do
-        expect(subject.total_score).to eq(15)
+  }
+
+  let(:real_data) { File.read("spec/fixtures/day02.txt") }
+
+  context "Part I" do
+    context "with test data" do
+      let(:input) { test_data }
+
+      describe "#straight_score" do
+        it "matches" do
+          expect(subject.straight_score).to eq(15)
+        end
+      end
+    end
+
+    context "with real data" do
+      let(:input) { real_data }
+
+      describe "#straight_score" do
+        it "matches" do
+          expect(subject.straight_score).to eq(14375)
+        end
       end
     end
   end
 
-  context "solve for real data" do
-    let(:input) { File.read("spec/fixtures/day02.txt") }
+  context "Part II" do
+    context "with test data" do
+      let(:input) { test_data }
 
-    describe "#total_score" do
-      it "solves part I" do
-        expect(subject.total_score).to eq(14375)
+      describe "#straight_score" do
+        it "matches" do
+          expect(subject.strat_score).to eq(12)
+        end
       end
     end
-  end
-end
 
-RSpec.describe Aoc2022::Day02::RockPaperScissorsPart2 do
-  let(:subject) { Aoc2022::Day02::RockPaperScissorsPart2.new(input) }
+    context "with real data" do
+      let(:input) { real_data }
 
-  context "solve for test data" do
-    let(:input) {
-      %(A Y
-B X
-C Z
-)
-    }
-    describe "#total_score" do
-      it "solves part II" do
-        expect(subject.total_score).to eq(12)
-      end
-    end
-  end
-
-  context "solve for real data" do
-    let(:input) { File.read("spec/fixtures/day02.txt") }
-
-    describe "#total_score" do
-      it "solves part II" do
-        expect(subject.total_score).to eq(10274)
+      describe "#straight_score" do
+        it "matches" do
+          expect(subject.strat_score).to eq(10274)
+        end
       end
     end
   end
