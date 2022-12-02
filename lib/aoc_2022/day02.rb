9b862677f4adc6013846bf031be9dea2cf94d46e
Davis W. Frank <dwfrank@gmail.com>
2022-12-02 13:46:16 -0800

    Only need one class

--- lib/aoc_2022/day02.rb
+++ lib/aoc_2022/day02.rb
@@ -1,55 +1,34 @@
 module Aoc2022
   module Day02
     class RockPaperScissors
-      def initialize(input)
-        @lines = input.split("\n")
-      end
-
-      def straight_score
-        score(StraightGame)
-      end
-
-      def strat_score
-        score(StratGame)
-      end
-
-      def score(game_klass)
-        @games = GameFactory.build(@lines, game_klass)
-        @games.sum { |g| g.score }
-      end
-    end
-
-    class GameFactory
-      def self.build(lines, klass)
-        lines.collect { |line| klass.new(line) }
-      end
-    end
-
-    class StraightGame
-      STRAIGHT_RESULT = {
+      STRAIGHT_SCORE = {
         "A X" => 4, "A Y" => 8, "A Z" => 3,
         "B X" => 1, "B Y" => 5, "B Z" => 9,
         "C X" => 7, "C Y" => 2, "C Z" => 6
       }
 
-      attr_reader :score
-
-      def initialize(line)
-        @score = STRAIGHT_RESULT[line]
-      end
-    end
-
-    class StratGame
-      STRAT_RESULT = {
+      STRAT_SCORE = {
         "A X" => 3, "A Y" => 4, "A Z" => 8,
         "B X" => 1, "B Y" => 5, "B Z" => 9,
         "C X" => 2, "C Y" => 6, "C Z" => 7
       }
 
-      attr_reader :score
+      def initialize(input)
+        @lines = input.split("\n")
+      end
 
-      def initialize(line)
-        @score = STRAT_RESULT[line]
+      def straight_score
+        score(STRAIGHT_SCORE)
+      end
+
+      def strat_score
+        score(STRAT_SCORE)
+      end
+
+      def score(scoring)
+        @lines.sum do |line|
+          scoring[line]
+        end
       end
     end
   end
