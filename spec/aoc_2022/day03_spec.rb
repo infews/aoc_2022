d3d35e3837df4c911a7e27daf72958c1d71102ce
Davis W. Frank <dwfrank@gmail.com>
2022-12-03 17:43:14 -0800

    Simplifies spec files for future days

--- spec/aoc_2022/day03_spec.rb
+++ spec/aoc_2022/day03_spec.rb
@@ -4,51 +4,34 @@ RSpec.describe Aoc2022::Day03::Baggage do
     Aoc2022::Day03::Baggage.new(lines)
   }
 
-  let(:test_data) {
-    %(vJrwpWtwJgWrhcsFMMfFFhFp
+  context "with test data" do
+    let(:data) {
+      %(vJrwpWtwJgWrhcsFMMfFFhFp
 jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
 PmmdzqPrVvPwwTWBwg
 wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
 ttgJtRGJQctTZtZT
 CrZsJsPPZsGzwwsLwLmpwMDw
 )
-  }
-
-  let(:real_data) { File.read("spec/fixtures/day03.txt") }
-
-  context "Part I" do
-    context "with test data" do
-      let(:data) { test_data }
-
-      it "solves #sum_sack_priority" do
-        expect(subject.sum_sack_priority).to eq(157)
-      end
+    }
+    it "solves Part I" do
+      expect(subject.sum_sack_priority).to eq(157)
     end
 
-    context "with real data" do
-      let(:data) { real_data }
-
-      it "solves #sum_sack_priority" do
-        expect(subject.sum_sack_priority).to eq(7831)
-      end
+    it "solves Part II " do
+      expect(subject.sum_badge_priority).to eq(70)
     end
   end
 
-  context "Part II" do
-    context "with test data" do
-      let(:data) { test_data }
+  context "with puzzle data" do
+    let(:data) { File.read("spec/fixtures/day03.txt") }
 
-      it "solves #sum_badge_priority" do
-        expect(subject.sum_badge_priority).to eq(70)
-      end
+    it "solves Part I" do
+      expect(subject.sum_sack_priority).to eq(7831)
     end
 
-    context "with real data" do
-      let(:data) { real_data }
-
-      it "solves #sum_badge_priority" do
-        expect(subject.sum_badge_priority).to eq(2683)
-      end
+    it "solves Part II " do
+      expect(subject.sum_badge_priority).to eq(2683)
     end
   end
 end
