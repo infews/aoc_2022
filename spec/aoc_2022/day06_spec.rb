877bc785b2afe87eab9cbf5a9b15a5c44b20385d
Davis W. Frank <dwfrank@gmail.com>
2022-12-06 21:19:10 -0800

    Inverting pending and running specs

--- spec/aoc_2022/day06_spec.rb
+++ spec/aoc_2022/day06_spec.rb
@@ -5,11 +5,11 @@ RSpec.describe Aoc2022::Day06::SignalDetector do
     context "example 1" do
       let(:data) { "mjqjpqmgbljsphdztnvjfqwrcgsmlb" }
 
-      xit "solves Part I" do
+      it"solves Part I" do
         expect(subject.start_of_packet).to eq(7)
       end
 
-      it "solves Part II" do
+      xit"solves Part II" do
         expect(subject.start_of_message).to eq(19)
       end
     end
@@ -17,11 +17,11 @@ RSpec.describe Aoc2022::Day06::SignalDetector do
     context "example 2" do
       let(:data) { "bvwbjplbgvbhsrlpgdmjqwftvncz" }
 
-      xit "solves Part I" do
+      it"solves Part I" do
         expect(subject.start_of_packet).to eq(5)
       end
 
-      xit "solves Part II" do
+      it"solves Part II" do
         expect(subject.start_of_message).to eq(23)
       end
     end
@@ -29,11 +29,11 @@ RSpec.describe Aoc2022::Day06::SignalDetector do
     context "example 3" do
       let(:data) { "nppdvjthqldpwncqszvftbrmjlhg" }
 
-      xit "solves Part I" do
+      it"solves Part I" do
         expect(subject.start_of_packet).to eq(6)
       end
 
-      xit "solves Part II" do
+      it"solves Part II" do
         expect(subject.start_of_message).to eq(23)
       end
     end
@@ -41,11 +41,11 @@ RSpec.describe Aoc2022::Day06::SignalDetector do
     context "example 4" do
       let(:data) { "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg" }
 
-      xit "solves Part I" do
+      it"solves Part I" do
         expect(subject.start_of_packet).to eq(10)
       end
 
-      xit "solves Part II" do
+      it"solves Part II" do
         expect(subject.start_of_message).to eq(29)
       end
     end
@@ -53,11 +53,11 @@ RSpec.describe Aoc2022::Day06::SignalDetector do
     context "example 5" do
       let(:data) { "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw" }
 
-      xit "solves Part I" do
+      it"solves Part I" do
         expect(subject.start_of_packet).to eq(11)
       end
 
-      xit "solves Part II" do
+      it"solves Part II" do
         expect(subject.start_of_message).to eq(26)
       end
     end
@@ -66,11 +66,11 @@ RSpec.describe Aoc2022::Day06::SignalDetector do
   context "with puzzle data" do
     let(:data) { File.read("spec/fixtures/day06.txt") }
 
-    xit "solves Part I" do
+    it"solves Part I" do
       expect(subject.start_of_packet).to eq(1647)
     end
 
-    xit "solves Part II" do
+    it"solves Part II" do
       expect(subject.start_of_message).to eq(2447)
     end
   end
