eea2cc3e8305b4524aedd550b0bf8fd029aeffdd
Davis W. Frank <dwfrank@gmail.com>
2022-12-10 16:01:27 -0800

    Day08 Refactoring

--- lib/aoc_2022/day08.rb
+++ lib/aoc_2022/day08.rb
@@ -1,9 +1,6 @@
 module Aoc2022
   module Day08
     class Forest
-      attr_reader :visible
-      attr_reader :scenic_score
-
       def initialize(input)
         @map = input.split("\n").collect do |s|
           s.chars.collect { |c| c.to_i }
@@ -11,59 +8,62 @@ module Aoc2022
 
         @width = @map.first.size
         @depth = @map.size
-        @visible = @width * 2 + @depth * 2 - 4
-        @scores = []
-        look
-        score
       end
 
-      def look
-        (1..(@depth - 2)).each do |row_index|
-          (1..(@width - 2)).each do |col_index|
-            current_tree = @map[row_index][col_index]
+      def visible
+        visible = @width * 2 + @depth * 2 - 4
 
-            top = (0..(row_index - 1)).collect { |i| @map[i][col_index] }
-            left = @map[row_index][0, col_index]
-            bottom = (@width - 1).downto(row_index + 1).collect { |i| @map[i][col_index] }
-            right = @map[row_index][col_index + 1, @depth - col_index]
+        two_d_loop do |row, col|
+          current_tree = @map[row][col]
 
-            visible = [top, left, bottom, right].collect do |trees|
-              trees.all? { |t| t < current_tree }
-            end
+          top = (0..(row - 1)).collect { |i| @map[i][col] }
+          left = @map[row][0, col]
+          bottom = (@width - 1).downto(row + 1).collect { |i| @map[i][col] }
+          right = @map[row][col + 1, @depth - col]
 
-            @visible += 1 if visible.any?(true)
+          seen = [top, left, bottom, right].collect do |trees|
+            trees.all? { |t| t < current_tree }
           end
+
+          visible += 1 if seen.any?(true)
         end
+        visible
       end
 
-      def score
-        (1..(@depth - 2)).each do |row_index|
-          (1..(@width - 2)).each do |col_index|
-            current_tree = @map[row_index][col_index]
+      def scenic_score
+        scores = []
+        two_d_loop do |row, col|
+          current_tree = @map[row][col]
 
-            top = (row_index - 1).downto(0).collect { |i| @map[i][col_index] }
-            left = (col_index - 1).downto(0).collect { |i| @map[row_index][i] }
-            bottom = ((row_index + 1)..(@width - 1)).collect { |i| @map[i][col_index] }
-            right = @map[row_index][col_index + 1, @depth - col_index]
+          top = (row - 1).downto(0).collect { |i| @map[i][col] }
+          left = (col - 1).downto(0).collect { |i| @map[row][i] }
+          bottom = ((row + 1)..(@width - 1)).collect { |i| @map[i][col] }
+          right = @map[row][col + 1, @depth - col]
 
-            a = [top, left, bottom, right].collect do |trees|
-              count_remaining_trees = true
-              trees.collect do |tree|
-                if (tree < current_tree) && count_remaining_trees
-                  1
-                elsif (tree >= current_tree) && count_remaining_trees
-                  count_remaining_trees = false
-                  1
-                else
-                  count_remaining_trees = false
-                  0
-                end
-              end.sum
-            end
-            @scores << a.inject(:*)
+          scores << [top, left, bottom, right].collect do |trees|
+            count_remaining_trees = true
+            trees.collect do |tree|
+              if (tree < current_tree) && count_remaining_trees
+                1
+              elsif (tree >= current_tree) && count_remaining_trees
+                count_remaining_trees = false
+                1
+              else
+                count_remaining_trees = false
+                0
+              end
+            end.sum
+          end.inject(:*)
+        end
+        scores.max
+      end
+
+      def two_d_loop
+        (1..(@depth - 2)).each do |row|
+          (1..(@width - 2)).each do |col|
+            yield row, col
           end
         end
-        @scenic_score = @scores.max
       end
     end
   end
